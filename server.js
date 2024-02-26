7b2dbd55e307eee8619805b47771775c2c683ed9
Ubuntu <wasimshaikh@k3d.rwx5e3ucmmpevlrpwoevz2ltzf.bx.internal.cloudapp.net>
2024-02-26 20:24:35 +0000

    Initial commit

---
+++ server.js
@@ -0,0 +1,15 @@
+// server.js
+
+const http = require('http');
+
+const PORT = process.env.PORT || 8080;
+
+const server = http.createServer((req, res) => {
+  res.writeHead(200, { 'Content-Type': 'text/plain' });
+  res.end('Hello from Node.js server!\n');
+});
+
+server.listen(PORT, () => {
+  console.log(`Server running at http://localhost:${PORT}/`);
+});
+
